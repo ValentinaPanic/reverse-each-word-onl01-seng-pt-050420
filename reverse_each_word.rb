75682d5333146a4582b1e34fb7e060cadc18d841
Valentina Panic <valentina.panic87@gmail.com>
2020-05-12 06:41:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(sentence1)
 new_sentence = []
   sentence_array = sentence1.split
   sentence_array.each do |word|
-     word.reverse
+     new_sentence << word.reverse
 
 end
+new_sentence.join(" ")
 end
